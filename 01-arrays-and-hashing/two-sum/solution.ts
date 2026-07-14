1e61b64a3c02f699ae532b4bf721575c35cba272
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-07-14 13:09:59 +0600

    feat: two sum

--- 01-arrays-and-hashing/two-sum/solution.ts
+++ 01-arrays-and-hashing/two-sum/solution.ts
@@ -2,35 +2,34 @@
 // O(1) space complexity
 
 function twoSum(nums: number[], target: number): number[] {
-    for(let i = 0; i < nums.length; i++) {
-        let currentElement = nums[i];
+  for (let i = 0; i < nums.length; i++) {
+    let currentElement = nums[i];
 
-        for(let j = i + 1; j < nums.length; j++) {
-            if(currentElement + nums[j] === target) {
-                return [i ,j]
-            }
-        }
+    for (let j = i + 1; j < nums.length; j++) {
+      if (currentElement + nums[j] === target) {
+        return [i, j];
+      }
     }
-    return []
-};
-
+  }
+  return [];
+}
 
 // O(n) time complexity
 // O(n) space complexity
-// function twoSum(nums: number[], target: number): number[] {
-//     const map = new Map();
+function twoSumWithMap(nums: number[], target: number): number[] {
+  const map = new Map();
 
-//     for(let i = 0; i < nums.length; i++) {
-//         const calculatedNum = target - nums[i];
-//         if(map.has(calculatedNum)) {
-//             return [map.get(calculatedNum), i]
-//         } else {
-//             map.set(nums[i], i)
-//         }
-//     }
-//     return []
-// };
+  for (let i = 0; i < nums.length; i++) {
+    const calculatedNum = target - nums[i];
+    if (map.has(calculatedNum)) {
+      return [map.get(calculatedNum), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+}
 
-console.log(twoSum([2,7,11,15], 9))
-console.log(twoSum([3,2,4], 6))
-console.log(twoSum([3,3], 6))
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
