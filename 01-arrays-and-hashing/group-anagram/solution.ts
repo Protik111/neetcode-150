a009ed8d182756efbed9ec1c09e61b506cf2edbe
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-07-23 11:22:13 +0600

    feat: group anagram with n*k complexity

--- 01-arrays-and-hashing/group-anagram/solution.ts
+++ 01-arrays-and-hashing/group-anagram/solution.ts
@@ -1,18 +1,18 @@
 // O(n * k log k) time complexity
 // O(n * k) space complexity
 
-// function groupAnagrams(strs: string[]): string[][] {
-//     const map = new Map<string, string[]>();
-//     for(let i = 0; i < strs.length; i++) {
-//         const sortedStr = strs[i].split("").sort().join("");
-//         if(map.has(sortedStr)) {
-//             map.get(sortedStr)!.push(strs[i])
-//         } else {
-//             map.set(sortedStr, [strs[i]])
-//         }
-//     }
-//     return Array.from(map.values())
-// };
+function groupAnagrams(strs: string[]): string[][] {
+  const map = new Map<string, string[]>();
+  for (let i = 0; i < strs.length; i++) {
+    const sortedStr = strs[i].split("").sort().join("");
+    if (map.has(sortedStr)) {
+      map.get(sortedStr)!.push(strs[i]);
+    } else {
+      map.set(sortedStr, [strs[i]]);
+    }
+  }
+  return Array.from(map.values());
+}
 
 // O(n * k) time complexity
 // O(n * k) space complexity
